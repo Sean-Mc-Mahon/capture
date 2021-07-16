c2fa54384aa37746502ead39638d75627a320fb2
Sean-Mc-Mahon <68223484+Sean-Mc-Mahon@users.noreply.github.com>
2021-07-16 15:33:38 +0100

    Individual movie component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 //Router
@@ -17,8 +18,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id" exact>
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
